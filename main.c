667adc106fca94f0d5233bc266e839d0ba17b1f7
Yuma Ohgami <yuma@ohgami.jp>
2020-01-05 13:01:01 +0900

    add write char sample.

--- main.c
+++ main.c
@@ -7,6 +7,14 @@
 #define PHNUM	1
 #define LOAD_VADDR	0x400000
 
+const char load_data_char_list[] =
+	"\n !\"#$%&'()*+,-./0123456789:;<=>?@"
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	"[\\]^_`"
+	"abcdefghijklmnopqrstuvwxyz"
+	"{|}~";
+static unsigned char load_data_char_list_len;
+
 const unsigned char load_data_loop[] = {
 	0xeb, 0xfe		/* jmp . */
 };
@@ -19,8 +27,20 @@ const unsigned char load_data_exit[] = {
 };
 #define LOAD_DATA_EXIT_SZ	16
 
-#define LOAD_DATA_SZ	LOAD_DATA_EXIT_SZ
-#define LOAD_DATA	load_data_exit
+const unsigned char load_data_write[] = {
+	0x48, 0xc7, 0xc7, 0x01, 0x00, 0x00, 0x00,	/* mov	$1,	%rdi */
+	0x48, 0xc7, 0xc6, 0x78, 0x00, 0x40, 0x00,	/* mov	$0x400078,%rsi*/
+	0x48, 0xc7, 0xc2, 0x01, 0x00, 0x00, 0x00,	/* mov	$1,	%rdx */
+	0x48, 0xc7, 0xc0, 0x01, 0x00, 0x00, 0x00,	/* mov	$1,	%rax */
+	0x0f, 0x05,					/* syscall */
+	0x48, 0x31, 0xff,				/* xor	%rdi,	%rdi */
+	0x48, 0xc7, 0xc0, 0x3c, 0x00, 0x00, 0x00,	/* mov	$60,	%rax */
+	0x0f, 0x05					/* syscall */
+};
+#define LOAD_DATA_WRITE_SZ	42
+
+#define LOAD_DATA_SZ	LOAD_DATA_WRITE_SZ
+#define LOAD_DATA	load_data_write
 
 static void write_elf64_ehdr(FILE *fp)
 {
@@ -48,7 +68,8 @@ static void write_elf64_ehdr(FILE *fp)
 	ehdr.e_shstrndx = SHN_UNDEF;
 
 	ehdr.e_entry =
-		LOAD_VADDR + sizeof(Elf64_Ehdr) + (sizeof(Elf64_Phdr) * PHNUM);
+		LOAD_VADDR + sizeof(Elf64_Ehdr) + (sizeof(Elf64_Phdr) * PHNUM)
+		+ load_data_char_list_len;
 
 	size_t n = fwrite(&ehdr, 1, sizeof(Elf64_Ehdr), fp);
 	if (n != sizeof(Elf64_Ehdr)) {
@@ -66,7 +87,7 @@ static void write_elf64_phdr(FILE *fp)
 	phdr.p_vaddr = LOAD_VADDR;
 	phdr.p_paddr = phdr.p_vaddr;
 	phdr.p_filesz = sizeof(Elf64_Ehdr) + (sizeof(Elf64_Phdr) * PHNUM)
-		+ LOAD_DATA_SZ;
+		+ load_data_char_list_len + LOAD_DATA_SZ;
 	phdr.p_memsz = phdr.p_filesz;
 	phdr.p_align = 0x200000;
 
@@ -79,7 +100,15 @@ static void write_elf64_phdr(FILE *fp)
 
 static void write_load_data(FILE *fp)
 {
-	size_t n = fwrite(LOAD_DATA, 1, LOAD_DATA_SZ, fp);
+	size_t n;
+
+	n = fwrite(load_data_char_list, 1, load_data_char_list_len, fp);
+	if (n != load_data_char_list_len) {
+		perror("fwrite(char list)");
+		exit(EXIT_FAILURE);
+	}
+
+	n = fwrite(LOAD_DATA, 1, LOAD_DATA_SZ, fp);
 	if (n != LOAD_DATA_SZ) {
 		perror("fwrite(load data)");
 		exit(EXIT_FAILURE);
@@ -88,6 +117,11 @@ static void write_load_data(FILE *fp)
 
 int main(void)
 {
+	printf("char list addr: 0x%08lx\n",
+	       LOAD_VADDR + sizeof(Elf64_Ehdr) + (sizeof(Elf64_Phdr) * PHNUM));
+
+	load_data_char_list_len = strlen(load_data_char_list);
+
 	FILE *fp = fopen(OUT_ELF_FILE, "w+b");
 	if (fp == NULL) {
 		perror("fopen");
